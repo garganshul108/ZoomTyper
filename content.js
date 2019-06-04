ea8ef7c0d9dcad7780232f6f2b9a63949ad8858e
garganshul108 <sci.agarg@gmail.com>
2019-06-04 16:48:19 +0530

    testing git

--- content.js
+++ content.js
@@ -4,7 +4,7 @@ for(i = 0 ; i < 10; i++)
     console.log("Go Anshul GO!!");
 }
 
-let arr = ["download.jpg"];
+let arr = ["download.jpg" , "alpha.jpg"];
 
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponce){
     console.log("message got");
@@ -15,7 +15,7 @@ let imgs = document.getElementsByTagName('img');
 
 for ( img of imgs ){
     console.log("Img: ", img.src);
-    let src = "kittens/"+arr[0];
+    let src = "kittens/"+arr[1];
     let url = chrome.extension.getURL(src); 
     img.src = url;
     console.log(img.src);
