3ea5ab627012766e63c9c19807deb4ace0d0e06b
garganshul108 <sci.agarg@gmail.com>
2019-06-04 16:36:50 +0530

    dealing with branches | mastr

--- content.js
+++ content.js
@@ -10,13 +10,3 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponce){
     console.log("message got");
     console.log(message.msg);
 });
-
-let imgs = document.getElementsByTagName('img');
-
-for ( img of imgs ){
-    console.log("Img: ", img.src);
-    let src = "kittens/"+arr[0];
-    let url = chrome.extension.getURL(src); 
-    img.src = url;
-    console.log(img.src);
-}
