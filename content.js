86bb56a5b7677aea79c432d627d8713ed59282b6
garganshul108 <sci.agarg@gmail.com>
2019-06-04 16:17:49 +0530

    adding resources | learning-extension

--- content.js
+++ content.js
@@ -4,7 +4,19 @@ for(i = 0 ; i < 10; i++)
     console.log("Go Anshul GO!!");
 }
 
+let arr = ["download.jpg"];
+
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponce){
     console.log("message got");
     console.log(message.msg);
 });
+
+let imgs = document.getElementsByTagName('img');
+
+for ( img of imgs ){
+    console.log("Img: ", img.src);
+    let src = "kittens/"+arr[0];
+    let url = chrome.extension.getURL(src); 
+    img.src = url;
+    console.log(img.src);
+}
